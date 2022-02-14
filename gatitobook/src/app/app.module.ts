3176e527fdcae40e17918f0308b0b7e94ba404ff
Thomas Erick <thomaserick.dev@gmail.com>
2022-02-13 22:36:41 -0300

    Footer

--- gatitobook/src/app/app.module.ts
+++ gatitobook/src/app/app.module.ts
@@ -4,10 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component';
+import { CabecalhoModule } from './componentes/cabecalho/cabecalho.module';
+import { RodapeComponent } from './componentes/rodape/rodape.component';
+import { RodapeModule } from './componentes/rodape/rodape.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    CabecalhoModule,
+    RodapeModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
