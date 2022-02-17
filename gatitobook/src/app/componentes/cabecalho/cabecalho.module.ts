c1d5caa942ad4362f7f384130bcf6ec85a910084
Thomas Erick <thomaserick.dev@gmail.com>
2022-02-17 17:35:39 -0300

    Menu

--- gatitobook/src/app/componentes/cabecalho/cabecalho.module.ts
+++ gatitobook/src/app/componentes/cabecalho/cabecalho.module.ts
@@ -1,11 +1,12 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { CabecalhoComponent } from './cabecalho.component';
+import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { MenuModule } from '../menu/menu.module';
+import { CabecalhoComponent } from './cabecalho.component';
 
 @NgModule({
   declarations: [CabecalhoComponent],
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, MenuModule],
   exports: [CabecalhoComponent],
 })
 export class CabecalhoModule {}
