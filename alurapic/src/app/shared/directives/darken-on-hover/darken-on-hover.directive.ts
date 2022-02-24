8add13d095708c80f79d3eb9a5d92d9ef89cbba7
Thomas Erick <thomas.erick@dev@gmail.com>
2022-02-24 11:48:09 -0300

    parametros para diretivas

--- alurapic/src/app/shared/directives/darken-on-hover/darken-on-hover.directive.ts
+++ alurapic/src/app/shared/directives/darken-on-hover/darken-on-hover.directive.ts
@@ -1,18 +1,28 @@
-import { Directive, ElementRef, HostListener, Renderer2 } from '@angular/core';
-
+import {
+  Directive,
+  ElementRef,
+  HostListener,
+  Input,
+  Renderer2,
+} from '@angular/core';
 @Directive({
   selector: '[appDarkenOnHover]',
 })
 export class DarkenOnHoverDirective {
+  @Input() brightness = '70%';
   constructor(private readonly el: ElementRef, private render: Renderer2) {}
 
   @HostListener('mouseover')
   darkOn() {
-    this.render.setStyle(this.el.nativeElement, 'filter', 'brightness(70%)');
+    this.render.setStyle(
+      this.el.nativeElement,
+      'filter',
+      `brightness(${this.brightness})`
+    );
   }
 
   @HostListener('mouseleave')
   darkOf() {
-    this.render.setStyle(this.el.nativeElement, 'filter', 'brightness(100%)');
+    this.render.setStyle(this.el.nativeElement, 'filter', `brightness(100%)`);
   }
 }
