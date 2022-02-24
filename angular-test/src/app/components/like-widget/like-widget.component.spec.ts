e7334fecc3d43e4d09d191ebf66ef061513dcea6
Thomas Erick <thomas.erick@dev@gmail.com>
2022-02-24 18:09:00 -0300

    ajuste imports

--- angular-test/src/app/components/like-widget/like-widget.component.spec.ts
+++ angular-test/src/app/components/like-widget/like-widget.component.spec.ts
@@ -1,16 +1,13 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { UniqueIdService } from 'src/app/shared/services/unique-id/unique-id.service';
 import { LikeWidgetComponent } from './like-widget.component';
+import { LikeWidgetModule } from './like-widget.module';
 
 describe(LikeWidgetComponent.name, () => {
   let fixture: ComponentFixture<LikeWidgetComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [LikeWidgetComponent],
-      providers: [UniqueIdService],
-      imports: [FontAwesomeModule],
+      imports: [LikeWidgetModule],
     }).compileComponents();
 
     fixture = TestBed.createComponent(LikeWidgetComponent);
@@ -18,5 +15,11 @@ describe(LikeWidgetComponent.name, () => {
 
   it('should create', () => {
     expect(fixture.componentInstance).toBeTruthy();
+  });
+
+  it('Should auto generate ID when id input property is missing', () => {
+    const component = fixture.componentInstance;
+
+    expect(component.id).toBeTruthy;
   });
 });
