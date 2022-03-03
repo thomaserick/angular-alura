2322553037a779e52dc08903056cd89954834fb8
Thomas Erick <thomaserick.dev@gmail.com>
2022-03-02 23:26:51 -0300

    Detectando a plataforma browser

--- alurapic/src/app/home/home.module.ts
+++ alurapic/src/app/home/home.module.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 import { SharedModule } from '../shared/shared.module';
 import { SigninComponent } from './signin/signin.component';
 
 @NgModule({
   declarations: [SigninComponent],
-  imports: [CommonModule, ReactiveFormsModule, SharedModule],
+  imports: [CommonModule, ReactiveFormsModule, SharedModule, RouterModule],
 })
 export class HomeModule {}
--- alurapic/src/app/home/signin/signin.component.ts
+++ alurapic/src/app/home/signin/signin.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/core/auth/auth.service';
+import { PlatformDetectorService } from 'src/app/core/platform-detector/platform-detector.service';
 
 @Component({
   selector: 'app-signin',
@@ -8,13 +11,35 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class SigninComponent implements OnInit {
   loginForm!: FormGroup;
+  @ViewChild('userNameInput') userNameInput!: ElementRef<HTMLInputElement>;
 
-  constructor(private readonly formBuild: FormBuilder) {}
+  constructor(
+    private readonly formBuild: FormBuilder,
+    private authService: AuthService,
+    private router: Router,
+    private readonly platformDetectionService: PlatformDetectorService
+  ) {}
 
   ngOnInit(): void {
     this.loginForm = this.formBuild.group({
       userName: ['', Validators.required],
       password: ['', Validators.required],
+    });
+  }
+
+  login() {
+    const userName = this.loginForm.get('userName')!.value;
+    const password = this.loginForm.get('password')!.value;
+
+    this.authService.authenticate(userName, password).subscribe({
+      next: () => {
+        this.router.navigate(['user', userName]);
+      },
+      error: () => {
+        this.loginForm.reset();
+        this.platformDetectionService.isPlatformBrowser() &&
+          this.userNameInput.nativeElement.focus();
+      },
     });
   }
 }
