c737e61158323103640a6a3df0414630eb7c46c1
Thomas Erick <thomaserick.dev@gmail.com>
2022-03-14 22:26:42 -0300

    Testes com Dom

--- angular-test-v2/src/app/shared/components/photo-frame/photo-frame.component.spec.ts
+++ angular-test-v2/src/app/shared/components/photo-frame/photo-frame.component.spec.ts
@@ -56,4 +56,38 @@ describe('PhotoFrameComponent', () => {
       fixture.nativeElement.querySelector('.like-counter');
     expect(element.textContent.trim()).toBe('1');
   });
+
+  it(`(D) Should display number of likes when (@Input likes) is incremented`, () => {
+    fixture.detectChanges();
+    component.likes++;
+    fixture.detectChanges();
+    const element: HTMLElement =
+      fixture.nativeElement.querySelector('.like-counter');
+    expect(element.textContent.trim()).toBe('1');
+  });
+
+  it(`(D) Should update aria-label when (@Input likes) is incremented`, () => {
+    fixture.detectChanges();
+    component.likes++;
+    fixture.detectChanges();
+    const element: HTMLElement = fixture.nativeElement.querySelector('span');
+    expect(element.getAttribute('aria-label')).toBe('1: people liked');
+  });
+
+  it(`(D) Should have aria-label with 0 (@Input likes)`, () => {
+    fixture.detectChanges();
+    const element: HTMLElement = fixture.nativeElement.querySelector('span');
+    expect(element.getAttribute('aria-label')).toBe('0: people liked');
+  });
+
+  it(`(D) Should display image with src and description when bound to properties`, () => {
+    const description = 'some description';
+    const src = 'http://somesite.com/img.jpg';
+    component.src = src;
+    component.description = description;
+    fixture.detectChanges();
+    const img: HTMLImageElement = fixture.nativeElement.querySelector('img');
+    expect(img.getAttribute('src')).toBe(src);
+    expect(img.getAttribute('alt')).toBe(description);
+  });
 });
