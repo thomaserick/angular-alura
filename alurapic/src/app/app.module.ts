17643c860dc9fa465f8b90443df5b45c6d6207f1
Thomas Erick <thomaserick.dev@gmail.com>
2022-03-16 22:41:24 -0300

    Componente header

--- alurapic/src/app/app.module.ts
+++ alurapic/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
 import { HomeModule } from './home/home.module';
 import { PhotosModule } from './photos/photos.module';
 import { SharedModule } from './shared/shared.module';
@@ -14,6 +15,7 @@ import { SharedModule } from './shared/shared.module';
     PhotosModule,
     SharedModule,
     HomeModule,
+    CoreModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
