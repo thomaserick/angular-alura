10d0f0a3c8d83bfca2a56a281f4840d1a3e6859c
Thomas Erick <thomaserick.dev@gmail.com>
2022-03-23 22:07:46 -0300

    Interceptor Token

---
+++ alurapic/src/app/core/auth/request.interceptor.ts
@@ -0,0 +1,31 @@
+import {
+  HttpEvent,
+  HttpHandler,
+  HttpInterceptor,
+  HttpRequest,
+} from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { TokenService } from '../token/token.service';
+
+@Injectable()
+export class RequestInterceptor implements HttpInterceptor {
+  constructor(private readonly tokenService: TokenService) {}
+
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+      
+    if (this.tokenService.hastoken()) {
+      const token = this.tokenService.getToken();
+      req = req.clone({
+        setHeaders: {
+          'x-access-token': token,
+        },
+      });
+    }
+
+    return next.handle(req);
+  }
+}
