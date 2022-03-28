731cb736f3df0771abb2f37d37b1a2a667909cc1
Thomas Erick <thomaserick.dev@gmail.com>
2022-03-28 20:37:22 -0300

    Validacao no formulario

--- alurapic/src/app/photos/photo-form/photo-form.component.ts
+++ alurapic/src/app/photos/photo-form/photo-form.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-photo-form',
   templateUrl: './photo-form.component.html',
-  styleUrls: ['./photo-form.component.scss']
+  styleUrls: ['./photo-form.component.scss'],
 })
 export class PhotoFormComponent implements OnInit {
+  photoForm!: FormGroup;
 
-  constructor() { }
+  constructor(private readonly formBuild: FormBuilder) {}
 
   ngOnInit(): void {
+    this.photoForm = this.formBuild.group({
+      file: ['', Validators.required],
+      description: ['', Validators.maxLength(300)],
+      allowComments: [true],
+    });
   }
-
 }
