2cde368a240e4d521a4a34b10e20bf8c27eb1da8
Thomas Erick <thomaserick.dev@gmail.com>
2022-07-04 23:03:17 -0300

    enviando parametros no decorator

--- typescript-3/app/dist/js/decorators/logar-tempo-execucao.js
+++ typescript-3/app/dist/js/decorators/logar-tempo-execucao.js
@@ -1,2 +1,19 @@
-export function logarTempoDeExecucao() {
+export function logarTempoDeExecucao(emSegundos = false) {
+    return function (target, propertyKey, description) {
+        const metodoOriginal = description.value;
+        description.value = function (...args) {
+            let devisor = 1;
+            let unidade = "milisegundos";
+            if (emSegundos) {
+                devisor = 1000;
+                unidade = "segundos";
+            }
+            const t1 = performance.now();
+            const retorno = metodoOriginal.apply(this, args);
+            const t2 = performance.now();
+            console.log(`${propertyKey}, tempo de execução: ${(t2 - t1) / devisor} ${unidade}`);
+            retorno;
+        };
+        return description;
+    };
 }
--- typescript-3/app/src/decorators/logar-tempo-execucao.ts
+++ typescript-3/app/src/decorators/logar-tempo-execucao.ts
@@ -1,4 +1,4 @@
-export function logarTempoDeExecucao() {
+export function logarTempoDeExecucao(emSegundos: boolean = false) {
   return function (
     target: any,
     propertyKey: string,
@@ -6,10 +6,18 @@ export function logarTempoDeExecucao() {
   ) {
     const metodoOriginal = description.value;
     description.value = function (...args: any[]) {
+      let devisor = 1;
+      let unidade = "milisegundos";
+      if (emSegundos) {
+        devisor = 1000;
+        unidade = "segundos";
+      }
       const t1 = performance.now();
       const retorno = metodoOriginal.apply(this, args);
       const t2 = performance.now();
-      console.log(`${propertyKey}, tempo de execução: ${(t1 - t2) / 100}`);
+      console.log(
+        `${propertyKey}, tempo de execução: ${(t2 - t1) / devisor} ${unidade}`
+      );
       retorno;
     };
 
