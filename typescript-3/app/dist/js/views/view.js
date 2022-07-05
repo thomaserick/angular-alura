bc5f7afd407d90ef11020880555529ffbfe5d12a
Thomas Erick <thomaserick.dev@gmail.com>
2022-07-04 22:56:51 -0300

    update

--- typescript-3/app/dist/js/views/view.js
+++ typescript-3/app/dist/js/views/view.js
@@ -13,11 +13,13 @@ export class View {
         }
     }
     update(model) {
+        const t1 = performance.now();
         let template = this.template(model);
         if (this.escapar) {
-            template = template
-                .replace(/<script>[\s\S]*?<\/script>/, '');
+            template = template.replace(/<script>[\s\S]*?<\/script>/, "");
         }
         this.elemento.innerHTML = template;
+        const t2 = performance.now();
+        console.log(`Tempo de execucao do método update: ${(t2 - t1) / 1000}`);
     }
 }
--- typescript-3/app/src/views/view.ts
+++ typescript-3/app/src/views/view.ts
@@ -1,28 +1,30 @@
 export abstract class View<T> {
+  protected elemento: HTMLElement;
+  private escapar = false;
 
-    protected elemento: HTMLElement;
-    private escapar = false;
-
-    constructor(seletor: string, escapar?: boolean) {
-        const elemento = document.querySelector(seletor);
-        if (elemento) {
-            this.elemento = elemento as HTMLElement;
-        } else {
-            throw Error(`Seletor ${seletor} não existe no DOM. Verifique`);
-        }
-        if (escapar) {
-            this.escapar = escapar;
-        }
+  constructor(seletor: string, escapar?: boolean) {
+    const elemento = document.querySelector(seletor);
+    if (elemento) {
+      this.elemento = elemento as HTMLElement;
+    } else {
+      throw Error(`Seletor ${seletor} não existe no DOM. Verifique`);
     }
-
-    public update(model: T): void {
-        let template = this.template(model);
-        if (this.escapar) {
-            template = template
-                .replace(/<script>[\s\S]*?<\/script>/, '');
-        }
-        this.elemento.innerHTML = template;
+    if (escapar) {
+      this.escapar = escapar;
     }
+  }
 
-    protected abstract template(model: T): string;
+  public update(model: T): void {
+    const t1 = performance.now();
+
+    let template = this.template(model);
+    if (this.escapar) {
+      template = template.replace(/<script>[\s\S]*?<\/script>/, "");
+    }
+    this.elemento.innerHTML = template;
+    const t2 = performance.now();
+    console.log(`Tempo de execucao do método update: ${(t2 - t1) / 1000}`);
+  }
+
+  protected abstract template(model: T): string;
 }
