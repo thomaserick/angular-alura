c58b4b633ad4b8e4a888f542390b7501a775871d
Thomas Erick <thomaserick.dev@gmail.com>
2022-07-06 21:00:39 -0300

    criando decorator escape

--- typescript-3/app/src/views/negociacoes-view.ts
+++ typescript-3/app/src/views/negociacoes-view.ts
@@ -1,10 +1,11 @@
-import { Negociacoes } from '../models/negociacoes.js';
-import { View } from './view.js';
+import { escapar } from "../decorators/escapar.js";
+import { Negociacoes } from "../models/negociacoes.js";
+import { View } from "./view.js";
 
 export class NegociacoesView extends View<Negociacoes> {
-
-    protected template(model: Negociacoes): string {
-        return `
+  @escapar
+  protected template(model: Negociacoes): string {
+    return `
         <table class="table table-hover table-bordered">
             <thead>
                 <tr>
@@ -14,7 +15,9 @@ export class NegociacoesView extends View<Negociacoes> {
                 </tr>
             </thead>
             <tbody>
-                ${model.lista().map(negociacao => {
+                ${model
+                  .lista()
+                  .map((negociacao) => {
                     return `
                         <tr>
                             <td>${this.formatar(negociacao.data)}
@@ -27,14 +30,14 @@ export class NegociacoesView extends View<Negociacoes> {
                             </td>
                         </tr>
                     `;
-                }).join('')}
+                  })
+                  .join("")}
             </tbody>
         </table>
         `;
-    }
+  }
 
-    private formatar(data: Date): string {
-        return new Intl.DateTimeFormat()
-            .format(data);
-    }
+  private formatar(data: Date): string {
+    return new Intl.DateTimeFormat().format(data);
+  }
 }
